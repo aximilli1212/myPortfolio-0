bf25d66f5a0d26e67994f7a12fef7f54a2722fef
Emmanuel Sackey <aximilli1212@gmail.com>
2023-10-13 06:04:30 +0000

    Add new assets for hero bg

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ const App = () => {
         <BrowserRouter>
             <div className='relative z-0 bg-primary'>
                 <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+                {/*<div className='bg-no-repeat bg-center'>*/}
                     <Navbar />
                     <Hero />
                 </div>
