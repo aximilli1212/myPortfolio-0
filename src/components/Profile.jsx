c930c016300d5d3767ff9b572320fcf84da35d02
Emmanuel Sackey <aximilli1212@gmail.com>
2023-10-13 07:18:35 +0000

    complete who i am

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -8,24 +8,30 @@ import {fadeIn, textVariant} from "../utils/motion";
 
 const Profile = () => {
   return (
-      <div className={`mt-2 p-8 bg-tertiary rounded-[20px] flex justify-between`}>
-          <div>
-                <img className="inline-block h-120 w-120 rounded-full ring-2 ring-black" src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
+      <div className="mt-2 p-8 bg-tertiary rounded-[20px] flex flex-wrap md:flex-nowrap justify-between items-center">
+          <div className="w-full md:w-auto md:mr-4">
+              <img
+                  className="h-30 w-30 rounded-full ring-2 ring-primary object-cover"
+                  src="src/assets/profile.png"
+                  alt=""
+              />
           </div>
-          <div className="p-2">
+          <div className="w-full md:w-auto text-center md:text-left">
               <motion.div variants={textVariant()}>
                   <h2 className={styles.sectionHeadText}>Who am i ?</h2>
               </motion.div>
 
               <motion.p
                   variants={fadeIn("", "", 0.1, 1)}
-                  className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+                  className="mt-4 text-secondary text-17 max-w-3xl leading-30"
               >
-                  I'm a skilled software developer with experience in TypeScript and
-                  JavaScript, and expertise in frameworks like React, Node.js, and
-                  Three.js. I'm a quick learner and collaborate closely with clients to
-                  create efficient, scalable, and user-friendly solutions that solve
-                  real-world problems. Let's work together to bring your ideas to life!
+                  I'm a skilled software developer with experience in Javascript, Typescript, Golang, Flutter and PHP.
+                  I also have expertise in frameworks like React, VueJS, Node.js, and Three.js.
+                  <br />
+                  <br />
+                  My diverse skill set, collaborative nature, and unwavering dedication to
+                  delivering exceptional customer experiences make me a valuable asset to any development team.
+                  Let's connect and explore how my expertise can contribute to your project.
               </motion.p>
           </div>
       </div>
