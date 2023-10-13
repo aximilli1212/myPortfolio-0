55be8af69d4910ce81e1c6ec83ffd489e1ad31a4
Emmanuel Sackey <aximilli1212@gmail.com>
2023-10-13 06:39:10 +0000

    add profile image

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -3,17 +3,31 @@ import { motion } from "framer-motion";
 
 import { styles } from "../styles";
 import { SectionWrapper } from "../hoc";
-import { textVariant } from "../utils/motion";
+import {fadeIn, textVariant} from "../utils/motion";
 
 
 const Profile = () => {
   return (
-      <div className={`mt-12 bg-tertiary rounded-[20px]`}>
-          <div className={`-mt-20 ${styles.paddingX} flex flex-wrap gap-7`}>
-            <div className="flex flex-row p-12">
-                <img className="inline-block h-100 w-100 rounded-full ring-2 ring-black" src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
-            </div>
-        </div>
+      <div className={`mt-2 p-8 bg-tertiary rounded-[20px] flex justify-between`}>
+          <div>
+                <img className="inline-block h-120 w-120 rounded-full ring-2 ring-black" src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
+          </div>
+          <div className="p-2">
+              <motion.div variants={textVariant()}>
+                  <h2 className={styles.sectionHeadText}>Who am i ?</h2>
+              </motion.div>
+
+              <motion.p
+                  variants={fadeIn("", "", 0.1, 1)}
+                  className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+              >
+                  I'm a skilled software developer with experience in TypeScript and
+                  JavaScript, and expertise in frameworks like React, Node.js, and
+                  Three.js. I'm a quick learner and collaborate closely with clients to
+                  create efficient, scalable, and user-friendly solutions that solve
+                  real-world problems. Let's work together to bring your ideas to life!
+              </motion.p>
+          </div>
       </div>
   );
 };
