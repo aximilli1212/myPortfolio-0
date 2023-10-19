07d1fd7dade752c33588162c22095ab76bec94f8
Emmanuel Sackey <aximilli1212@gmail.com>
2023-10-19 13:25:34 +0000

    update my name

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,7 +16,7 @@ const Hero = () => {
 
           <div>
             <h1 className={`${styles.heroHeadText} text-white`}>
-              Hi, I'm <span className='text-gray-100'>Nii</span>
+              Hi, I'm <span className='text-gray-100'>Emmanuel</span>
             </h1>
             <p className={`${styles.heroSubText} mt-2 text-white-100`}>
                 A passionate Senior Software Developer
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -44,7 +44,7 @@ const Navbar = () => {
           >
             <img src={logo} alt='logo' className='w-9 h-9 object-contain shadow-2xl' />
             <p className='text-white text-[18px] font-bold cursor-pointer flex ml-2'>
-              Nii Sakie &nbsp;
+              Emmanuel Sackey &nbsp;
               <span className='sm:block hidden'> | Software Developer</span>
             </p>
           </Link>
